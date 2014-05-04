260eae0df12c922c56b4016fd906734c39c3e25c
Ted Marshall <ted@33degreesDS.com>
2014-05-04 12:21:30 -0700

    Updated CSS
    Adjusting after Ben’s Updates

--- footer.php
+++ footer.php
@@ -54,20 +54,20 @@
 </footer>
 <?php
 
-//---------- Google Analytics ---------// ?>
-<script type="text/javascript">
+//---------- Google Analytics ---------//
+// <script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-279258-17']);
-  _gaq.push(['_trackPageview']);
+//   var _gaq = _gaq || [];
+//   _gaq.push(['_setAccount', 'UA-279258-17']);
+//   _gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+//  (function() {
+//    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+//    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+//    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+//  })();
 
-</script>
+//  </script> ?>
 
 <?php wp_footer(); ?>
 
